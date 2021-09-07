c73b55594410971ded6684457a50dcbad1301f56
Daniele R.C <darthreca@gmail.com>
2021-09-07 18:19:52 +0200

    Get by UID work and completed parsing of VTODO

--- CalendarToDoList/CalendarClient/CalendarClient.cpp
+++ CalendarToDoList/CalendarClient/CalendarClient.cpp
@@ -225,47 +225,49 @@ QNetworkReply* CalendarClient::getAllElements() {
                                             xml.toByteArray());
 }
 
-QNetworkReply *CalendarClient::getElementByUID(QString UID)
-{
-    if (!supportedMethods_.contains("REPORT")) {
-      qDebug() << "Method REPORT not supported in call getElementByUID";
-      return nullptr;
-    }
+QNetworkReply* CalendarClient::getElementByUID(QString UID) {
+  if (!supportedMethods_.contains("REPORT")) {
+    qDebug() << "Method REPORT not supported in call getElementByUID";
+    return nullptr;
+  }
 
-    QNetworkRequest cal_part;
-    cal_part.setRawHeader("Authorization", ("Basic " + credentials_));
-    cal_part.setUrl(QUrl(endpoint_));
-    cal_part.setRawHeader("Host", "cal.example.com");
-    cal_part.setRawHeader("Depth", "1");
-    cal_part.setHeader(QNetworkRequest::KnownHeaders::ContentTypeHeader,
-                       "application/xml; charset=utf-8");
-    cal_part.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader,
-                       "CalendarClient_CalDAV");
+  QNetworkRequest cal_part;
+  cal_part.setRawHeader("Authorization", ("Basic " + credentials_));
+  cal_part.setUrl(QUrl(endpoint_));
+  cal_part.setRawHeader("Depth", "1");
+  cal_part.setRawHeader("Prefer", "return-minimal");
+  cal_part.setHeader(QNetworkRequest::KnownHeaders::ContentTypeHeader,
+                     "application/xml; charset=utf-8");
+  cal_part.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader,
+                     "CalendarClient_CalDAV");
 
-    QDomDocument xml;
-    QDomElement root = xml.createElement("c:calendar-query");
-    root.setAttribute("xmlns:c", "urn:ietf:params:xml:ns:caldav");
-    xml.appendChild(root);
-    QDomElement tagProp = xml.createElement("d:prop");
-    tagProp.setAttribute("xmlns:d", "DAV:");
-    tagProp.appendChild(xml.createElement("d:getetag"));
-    tagProp.appendChild(xml.createElement("c:calendar-data"));
-    root.appendChild(tagProp);
-    QDomElement tagFilter = xml.createElement("c:filter");
-    QDomElement tagCompFilter = xml.createElement("c:comp-filter");
-    tagCompFilter.setAttribute("name", "VCALENDAR");
-    tagFilter.appendChild(tagCompFilter);
-    QDomElement tagPropFilter = xml.createElement("c:prop-filter");
-    tagPropFilter.setAttribute("name", "UID");
-    tagCompFilter.appendChild(tagPropFilter);
-    QDomElement tagTextMatch = xml.createElement("c:text-match");
-    tagPropFilter.setAttribute("collation", "i;octet");
-    tagTextMatch.appendChild(xml.createTextNode(UID));
-    tagPropFilter.appendChild(tagTextMatch);
-    root.appendChild(tagFilter);
+  QDomDocument xml;
+  QDomElement root = xml.createElement("c:calendar-query");
+  root.setAttribute("xmlns:c", "urn:ietf:params:xml:ns:caldav");
+  xml.appendChild(root);
+  QDomElement tagProp = xml.createElement("d:prop");
+  tagProp.setAttribute("xmlns:d", "DAV:");
+  tagProp.appendChild(xml.createElement("d:getetag"));
+  tagProp.appendChild(xml.createElement("c:calendar-data"));
+  root.appendChild(tagProp);
+  QDomElement tagFilter = xml.createElement("c:filter");
+  QDomElement tagCompFilter = xml.createElement("c:comp-filter");
+  tagCompFilter.setAttribute("name", "VCALENDAR");
+  tagFilter.appendChild(tagCompFilter);
+  QDomElement tag_comp_filter2 = xml.createElement("c:comp-filter");
+  tag_comp_filter2.setAttribute("name", "VEVENT");
+  tagCompFilter.appendChild(tag_comp_filter2);
+  QDomElement tagPropFilter = xml.createElement("c:prop-filter");
+  tagPropFilter.setAttribute("name", "UID");
+  tag_comp_filter2.appendChild(tagPropFilter);
+  QDomElement tagTextMatch = xml.createElement("c:text-match");
+  tagPropFilter.setAttribute("collation", "i;octet");
+  tagTextMatch.appendChild(xml.createTextNode(UID));
+  tagPropFilter.appendChild(tagTextMatch);
+  root.appendChild(tagFilter);
 
-    return network_manager_.sendCustomRequest(cal_part, QByteArray("REPORT"),
-                                              xml.toByteArray());
+  return network_manager_.sendCustomRequest(cal_part, QByteArray("REPORT"),
+                                            xml.toByteArray());
 }
 
 QNetworkReply* CalendarClient::lookForChanges() {
--- CalendarToDoList/calendar_classes/task.cpp
+++ CalendarToDoList/calendar_classes/task.cpp
@@ -1,42 +1,41 @@
 #include "task.h"
 
-Task &Task::fromICalendar(QTextStream &icalendar)
-{
-    for (QString line = icalendar.readLine(); !line.contains("END:VTODO");
-         line = icalendar.readLine()) {
-      QStringList key_value = line.split(":");
-      if (key_value.size() != 2) continue;
-      QString key = key_value[0];
-      QString value = key_value[1].replace("\\n", "\n");
+Task &Task::fromICalendar(QTextStream &icalendar) {
+  for (QString line = icalendar.readLine(); !line.contains("END:VTODO");
+       line = icalendar.readLine()) {
+    QStringList key_value = line.split(":");
+    if (key_value.size() != 2) continue;
+    QString key = key_value[0];
+    QString value = key_value[1].replace("\\n", "\n");
 
-      QString testEncodingString = value.toUtf8();
-      if (!testEncodingString.contains("�")) value = testEncodingString;
+    QString testEncodingString = value.toUtf8();
+    if (!testEncodingString.contains("�")) value = testEncodingString;
 
-      if (key.startsWith(QLatin1String("DTSTART"))) {
-        setStartDateTime(dateTimeFromString(value).toLocalTime());
-      } else if (key.startsWith(QLatin1String("DTEND"))) {
-        setEndDateTime(dateTimeFromString(value).toLocalTime());
-      } else if (key.startsWith(QLatin1String("DUE"))) {
-        QDateTime date_time = dateTimeFromString(value).toLocalTime();
-        setStartDateTime(date_time);
-        setEndDateTime(date_time);
-      } else if (key == QLatin1String("RRULE")) {
-        setRRULE(value);
-      } else if (key == QLatin1String("EXDATE")) {
-        setExdates(value);
-      } else if (key == QLatin1String("SUMMARY")) {
-        setSummary(value);
-      } else if (key == QLatin1String("LOCATION")) {
-        setLocation(value);
-      } else if (key == QLatin1String("UID")) {
-        setUid(value);
-      } else if (key == QLatin1String("CATEGORIES")) {
-        setCategories(value);
-      } else if (key == QLatin1String("DESCRIPTION")) {
-        setDescription(value);
-      }
+    if (key.startsWith(QLatin1String("DTSTART"))) {
+      setStartDateTime(dateTimeFromString(value).toLocalTime());
+    } else if (key.startsWith(QLatin1String("DUE"))) {
+      QDateTime date_time = dateTimeFromString(value).toLocalTime();
+      setStartDateTime(date_time);
+      setEndDateTime(date_time);
+    } else if (key == QLatin1String("RRULE")) {
+      setRRULE(value);
+    } else if (key == QLatin1String("EXDATE")) {
+      setExdates(value);
+    } else if (key == QLatin1String("SUMMARY")) {
+      setSummary(value);
+    } else if (key == QLatin1String("LOCATION")) {
+      setLocation(value);
+    } else if (key == QLatin1String("UID")) {
+      setUid(value);
+    } else if (key == "CATEGORIES") {
+      setCategories(value);
+    } else if (key == "DESCRIPTION") {
+      setDescription(value);
+    } else if (key == "COMPLETED") {
+      completed_ = QPair<bool, QDateTime>(true, dateTimeFromString(value));
     }
-    return *this;
+  }
+  return *this;
 }
 
 QString Task::toICalendar() {
@@ -80,4 +79,4 @@ void Task::flipCompleted() {
   completed_.second = QDateTime::currentDateTime();
 }
 
-const QPair<bool, QDateTime>& Task::completed() const { return completed_; }
+const QPair<bool, QDateTime> &Task::completed() const { return completed_; }
